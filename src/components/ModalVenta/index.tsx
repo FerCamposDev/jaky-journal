da8c4df42b8e89d234f296d3dc95f36d8c84541d
FerCamposDev <fernando.developments@gmail.com>
2022-08-24 23:54:28 -0300

    seleccionador de billetes

--- src/components/ModalVenta/index.tsx
+++ src/components/ModalVenta/index.tsx
@@ -1,16 +1,19 @@
-import { ChangeEvent, FC, useState } from 'react';
+import {
+  ChangeEvent, FC, useEffect, useState,
+} from 'react';
 import Button from '@mui/material/Button';
 import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
 import { Usuario } from 'types/usuario';
 import { Venta } from 'types/venta';
 import { generarVenta } from 'utils/generadores';
 import { useDolar } from 'contexts/DolarContext';
 import dayjs from 'dayjs';
+import lodash from 'lodash';
+import SeleccionadorBilletes from './SeleccionadorBilletes';
 
 type Props = {
   usuario: Usuario;
@@ -20,6 +23,16 @@ const ModalVenta: FC<Props> = ({ usuario }) => {
   const [open, setOpen] = useState(false);
   const { precio } = useDolar();
   const [nuevaVenta, setNuevaVenta] = useState<Venta>(generarVenta(usuario, precio));
+  const [billetes, setBilletes] = useState<number[]>([]);
+  const [billetesRetorno, setBilletesRetorno] = useState<number[]>([]);
+
+  useEffect(() => {
+    setNuevaVenta({
+      ...nuevaVenta,
+      billetes,
+      billetesRetorno,
+    });
+  }, [billetes, billetesRetorno]);
 
   const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
     const { name, value } = e.target;
@@ -42,16 +55,6 @@ const ModalVenta: FC<Props> = ({ usuario }) => {
           valorDolar: Number(value),
           cantidadUsd: nuevaVenta.totalPesos / Number(value),
         });
-      case 'billetes':
-        return setNuevaVenta({
-          ...nuevaVenta,
-          billetes: value.split(',').map((elem) => Number(elem)),
-        });
-      case 'billetesRetorno':
-        return setNuevaVenta({
-          ...nuevaVenta,
-          billetesRetorno: value.split(',').map((elem) => Number(elem)),
-        });
       default: break;
     }
   };
@@ -62,6 +65,27 @@ const ModalVenta: FC<Props> = ({ usuario }) => {
 
   const handleClose = () => {
     setOpen(false);
+  };
+
+  const guardarVenta = async () => {
+    const {
+      cantidadUsd, comprador, totalPesos, valorDolar,
+    } = nuevaVenta;
+    const sumaBilletes = lodash.sum(billetes);
+    const sumaVuelto = lodash.sum(billetesRetorno);
+    if (!cantidadUsd || !comprador || !valorDolar) {
+      alert('faltan datos');
+    }
+    if (sumaBilletes - sumaVuelto !== Math.round(totalPesos / valorDolar)) {
+      console.log('sumaBilletes -sumaVuelto', sumaBilletes - sumaVuelto);
+      console.log('Math.round(totalPesos/valorDolar) :>> ', Math.round(totalPesos / valorDolar));
+      alert('los billetes no son igual que el total');
+    }
+    try {
+      // await postVenta(nuevaVenta);
+    } catch (error) {
+      console.log('error', error);
+    }
   };
 
   return (
@@ -75,10 +99,6 @@ const ModalVenta: FC<Props> = ({ usuario }) => {
           {usuario.nombre}
         </DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
           <TextField
             label="Día de venta"
             name="fecha"
@@ -113,26 +133,12 @@ const ModalVenta: FC<Props> = ({ usuario }) => {
             variant="outlined"
             label="Cantidad de USD"
           />
-          <TextField
-            fullWidth
-            value={nuevaVenta.billetes.toString()}
-            variant="outlined"
-            name="billetes"
-            label="Billetes (separados por coma)"
-            onChange={handleChange}
-          />
-          <TextField
-            fullWidth
-            value={nuevaVenta.billetesRetorno.toString()}
-            variant="outlined"
-            name="billetesRetorno"
-            label="Vuelto (separados por coma)"
-            onChange={handleChange}
-          />
+          <SeleccionadorBilletes placeholder="Seleccione los billetes" billetes={billetes} setBilletes={setBilletes} />
+          <SeleccionadorBilletes placeholder="Seleccione los billetes de vuelto" billetes={billetesRetorno} setBilletes={setBilletesRetorno} />
         </DialogContent>
         <DialogActions>
           <Button onClick={handleClose}>Cancelar</Button>
-          <Button onClick={handleClose} variant="contained">Guardar</Button>
+          <Button onClick={guardarVenta} variant="contained">Guardar</Button>
         </DialogActions>
       </Dialog>
     </div>
